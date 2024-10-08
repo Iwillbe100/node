4372bec897974ef6badd9df0b87f4095c303f025
a1163123 <a1163123@gmail.com>
2024-10-08 20:15:47 +0900

    @

--- web03/routes/users.js
+++ web03/routes/users.js
@@ -4,6 +4,18 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  
 });
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'login', pageName:'users/login.ejs' });
+});
+
+/* GET home page. */
+router.get('/join', function(req, res, next) {
+  res.render('index', { title: '회원가입', pageName:'users/join.ejs' });
+});
+
 
 module.exports = router;
